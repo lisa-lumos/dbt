ce2660e850d6cbd0f0e6155b27402b5b20295c9a
lisa-lumos <zjcty307@gmail.com>
2023-06-07 14:17:09 -0600

    add dbt snapshots

---
+++ 01-dbt-core/code/dbtlearn/snapshots/scd_raw_hosts.sql
@@ -0,0 +1,18 @@
+{% snapshot scd_raw_hosts %}
+
+{{
+  config(
+    target_schema='dev',
+    unique_key='id',
+    strategy='timestamp',
+    updated_at='updated_at',
+    invalidate_hard_deletes=true
+  )
+}}
+
+select 
+  * 
+from 
+  {{ source('airbnb', 'hosts') }}
+
+{% endsnapshot %}
